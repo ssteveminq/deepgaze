aa07bbf783d6a5ba10264ea46d6977cb16f9a620
minkyukim <steveminq@gmail.com>
2019-03-14 23:32:25 -0500

    predict_pointset updated

--- deepgaze/object3d_tracking.py
+++ deepgaze/object3d_tracking.py
@@ -18,7 +18,7 @@ from numpy.random import uniform
 import cv2
 import sys
 
-class ParticleFilter:
+class ParticleFilter3D:
     """Particle filter motion tracking.
 
     This class estimates the position of a single point in a 3d space.
@@ -83,6 +83,33 @@ class ParticleFilter:
             
         # print "predict context"
         # self.particles[:, 2] += z_velocity + (np.random.randn(len(self.particles)) * std) #predict the Y coord
+    def predict_point(self,point,std ):
+        """Predict the position of the point in the next frame from context information.
+        Move the particles based on how the real system is predicted to behave.
+        """
+        self.particles[:, 0] = point.x + (np.random.randn(len(self.particles)) * std) #predict the X coord
+        self.particles[:, 1] = point.y + (np.random.randn(len(self.particles)) * std) #predict the Y coord
+            
+        # print "predict context"
+        # self.particles[:, 2] += z_velocity + (np.random.randn(len(self.particles)) * std) #predict the Y coord
+
+    def predict_pointset(self,alpha, pointset,std ):
+        """Predict the position of the point in the next frame from context information.
+        Move the particles based on how the real system is predicted to behave.
+        """
+        num_particles = len(self.particles)
+        half_num_particles = int(0.5*num_particles)
+        for id in range(half_num_particles):
+            self.particles[id, 0] = pointset[0].x + uniform(-0.2,0.2)
+            self.particles[id, 1] = pointset[0].y + uniform(-0.2,0.2)
+            half_id = id + half_num_particles 
+            self.particles[half_id, 0] = pointset[1].x + uniform(-0.2,0.2)
+            self.particles[half_id, 1] = pointset[1].y + uniform(-0.2,0.2)
+            
+        # print "predict context"
+        # self.particles[:, 2] += z_velocity + (np.random.randn(len(self.particles)) * std) #predict the Y coord
+
+
 
 
     # def update(self, x, y,z):
